73fe0aaa45d99c000b2c522f40a00792459d1c15
extmzig <mans.zigher@mikrodidakt.se>
2023-09-06 21:17:49 +0200

    Adding more tests for executer

--- src/commands/tests.rs
+++ src/commands/tests.rs
@@ -4,7 +4,16 @@ mod tests {
     use crate::error::BError;
     use crate::cli::*;
     use crate::workspace::Workspace;
+    use crate::docker::{Docker, DockerImage};
     
+    fn helper_test_executer(verification_str: &String, test_cmd: &String, test_build_dir: Option<String>, docker: Option<Docker>, workspace: &Workspace) -> Result<(), BError> {
+        let mut mocked_logger: MockLogger = MockLogger::new();
+        mocked_logger.expect_info().with(mockall::predicate::eq(verification_str.clone())).once().returning(|_x|());
+        let cli: Cli = Cli::new(Box::new(mocked_logger));
+        let exec: Executer = Executer::new(workspace, &cli);
+        exec.execute(&test_cmd, std::env::vars(), test_build_dir, docker, true) 
+    }
+
     #[test]
     fn test_build_command() {
         let cmd: BuildCommand = BuildCommand::new();
@@ -64,12 +73,54 @@ mod tests {
     }
 
     #[test]
-    fn test_executer() {
-        let mut mocked_logger: MockLogger = MockLogger::new();
-        mocked_logger.expect_info().with(mockall::predicate::eq("Execute 'cd test2 && test'".to_owned())).once().returning(|_x|());
-        let workspace: Workspace = Workspace{ _work_dir: String::from("test") };
-        let cli: Cli = Cli::new(Box::new(mocked_logger));
-        let exec: Executer = Executer::new(&workspace, &cli);
-        let _result: Result<(), BError> = exec.execute("test", std::env::vars(), Some(String::from("test2")), None, true); 
+    fn test_executer_build_dir() {
+        let test_work_dir = String::from("test_work_dir");
+        let test_build_dir = String::from("test_build_dir");
+        let test_cmd = String::from("test_cmd");
+        let verification_str = format!("Execute 'cd {} && {}'", test_build_dir, test_cmd);
+        let workspace: Workspace = Workspace{ _work_dir: test_work_dir };
+        let result: Result<(), BError> = helper_test_executer(&verification_str, &test_cmd, Some(test_build_dir), None, &workspace);
+        match result {
+            Err(err) => {
+                assert_eq!("Executer failed", err.message);
+            }
+            Ok(()) => {}
+        }
+    }
+
+    #[test]
+    fn test_executer_no_build_dir() {
+        let test_work_dir = String::from("test_work_dir");
+        let test_cmd = String::from("test_cmd");
+        let verification_str = format!("Execute 'cd {} && {}'", test_work_dir, test_cmd);
+        let workspace: Workspace = Workspace{ _work_dir: test_work_dir };
+        let result: Result<(), BError> = helper_test_executer(&verification_str, &test_cmd, None, None, &workspace);
+        match result {
+            Err(err) => {
+                assert_eq!("Executer failed", err.message);
+            }
+            Ok(()) => {}
+        }
+    }
+
+    #[test]
+    fn test_executer_docker() {
+        let test_work_dir = String::from("test_work_dir");
+        let test_cmd = String::from("test_cmd");
+        let docker_image: DockerImage = DockerImage {
+            registry: String::from("test-registry"),
+            image: String::from("test-image"),
+            tag: String::from("0.1"),
+        };
+        let verification_str = format!("Execute inside docker image {} 'cd {} && {}'", docker_image, test_work_dir, test_cmd);
+        let workspace: Workspace = Workspace{ _work_dir: test_work_dir };
+        let docker: Docker = Docker::new(&workspace, &docker_image, true);
+        let result: Result<(), BError> = helper_test_executer(&verification_str, &test_cmd, None, Some(docker), &workspace);
+        match result {
+            Err(err) => {
+                assert_eq!("Executer failed", err.message);
+            }
+            Ok(()) => {}
+        }
     }
 }
