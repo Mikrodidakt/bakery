0af2add2213d2f78a3c93ccf722f0095645d68d9
extmzig <mans.zigher@mikrodidakt.se>
2023-09-07 20:07:41 +0200

    Adding tests for settings

--- src/workspace/settings.rs
+++ src/workspace/settings.rs
@@ -1,20 +1,100 @@
 use serde_json::Value;
+
+/*
+The settings file named settings.json or workspace.json should be placed
+in the root of the workspace. The current format is
+        {
+            "version": 4,
+            "workspace": {
+              "configsdir": "configs_test", // Location for build configs default is ${BAKERY_WORKSPACE}/configs
+              "buildsdir": "builds_test", // This is the bitbake build directory default is ${BAKERY_WORKSPACE}/builds
+              "artifactsdir": "artifacts_test", // This is the location where bakery will place all artifacts defaul is ${BAKERY_WORKSPACE}/artifacts
+              "scriptsdir": "scripts_test", // Each build component might expect to find scripts this default is ${BAKERY_WORKSPACE}/scripts
+              "dockerdir": "scripts/docker_test", //
+              "cachedir": ".cache_test" // This is the cache directory for sstate and dl dir for bitbake default is ${BAKERY_WORKSPACE}/cache
+            },
+            "build": {
+              "supported": [
+                "machine1-test",
+                "machine2-test"
+              ]
+            },
+            "docker": {
+              "registry": "test",
+              "image": "test-workspace",
+              "tag": "0.1",
+              "args": [
+                "--rm=true",
+                "-t",
+                "--dns=8.8.8.8"
+              ]
+            }
+          }
+        }
+
+The workspace node is for changing the default directories in the workspace. Only needs to be set
+if the defaults should be changed. The build node is for defining what build configs are enabled
+in the workspace if nothing is specified all build configs are available to use. The docker node
+is for configuring the initial docker image that will be used to bootstrap the bakery. This is
+the main docker image used so unless the build config contains specific docker images for a component
+then this image will be used when building.
+*/
 pub struct Settings {
-    data: Value, 
+    workspace: Value,
+    docker: Value,
+    build: Value,
 }
 
 impl Settings {
     pub fn from_str(json_string: &str) -> Result<Self, serde_json::Error> {
         let data: Value = serde_json::from_str(json_string)?;
-        Ok(Settings { data })
+        //let version: String = String::from(data["version"].as_str().unwrap());
+        let workspace: Value = data["workspace"].clone();
+        let docker: Value = data["docker"].clone();
+        let build: Value = data["build"].clone();
+        Ok(Settings { workspace, docker, build })
     }
 
-    pub fn print(&self) {
-        println!("data {}", self.data);
+    pub fn workspace_dir(&self) -> String {
+        return String::from("work_dir");
     }
 
-    pub fn workspace_configs_dir(&self) -> &str {
-        &self.data["workspace"]["configsdir"].as_str().unwrap()
+    fn get_ws_dir(&self, dir: &str) -> String {
+        let work_dir = self.workspace_dir();
+        // TODO: we should consider to maybe have the default cache dir as .cache
+        let key = format!("{}dir", dir);
+        let default_value = format!("{}/{}", work_dir, dir);
+        if !self.workspace.is_null() {
+            if self.workspace[key.clone()].is_null() {
+                return default_value;
+            }
+            return format!("{}/{}", work_dir, self.workspace[key.clone()].as_str().unwrap());
+        }
+        return default_value;    
+    }
+
+    pub fn workspace_configs_dir(&self) -> String {
+        self.get_ws_dir("configs")
+    }
+
+    pub fn workspace_builds_dir(&self) -> String {
+        self.get_ws_dir("builds")
+    }
+
+    pub fn workspace_artifacts_dir(&self) -> String {
+        self.get_ws_dir("artifacts")
+    }
+
+    pub fn workspace_scripts_dir(&self) -> String {
+        self.get_ws_dir("scripts")
+    }
+
+    pub fn workspace_docker_dir(&self) -> String {
+        self.get_ws_dir("docker")
+    }
+
+    pub fn workspace_cache_dir(&self) -> String {
+        self.get_ws_dir("cache")
     }
 }
 
--- src/workspace/tests.rs
+++ src/workspace/tests.rs
@@ -2,37 +2,7 @@
 mod tests {
     use crate::workspace::Settings;
 
-    #[test]
-    fn test_settings_build_dir() {
-        let json_test_str = r#"
-        {
-            "version": 4,
-            "workspace": {
-              "configsdir": "configs_test",
-              "buildsdir": "builds_test",
-              "artifactsdir": "artifacts_test",
-              "scriptsdir": "scripts_test",
-              "dockerdir": "scripts/docker_test",
-              "cachedir": ".cache_test"
-            },
-            "build": {
-              "supported": [
-                "machine1-test",
-                "machine2-test"
-              ]
-            },
-            "docker": {
-              "registry": "test",
-              "image": "test-workspace",
-              "tag": "0.1",
-              "args": [
-                "--rm=true",
-                "-t",
-                "--dns=8.8.8.8"
-              ]
-            }
-          }
-        "#;
+    fn helper_settings_from_str(json_test_str: &str) -> Settings {
         let result: Result<Settings, serde_json::Error> = Settings::from_str(json_test_str);
         let settings: Settings;
         match result {
@@ -44,6 +14,222 @@ mod tests {
                 panic!();
             } 
         }
-        assert_eq!(settings.workspace_configs_dir(), "configs_test");
+        settings
+    }
+
+    #[test]
+    fn test_settings_configs_dir() {
+        let json_test_str = r#"
+        {
+            "version": 4,
+            "workspace": {
+              "configsdir": "configs_test"
+            }
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_configs_dir(),  "work_dir/configs_test");
+    }
+
+    #[test]
+    fn test_settings_no_configs_dir() {
+        let json_test_str = r#"
+        {
+            "version": 4,
+            "workspace": {
+              "artifactsdir": "artifacts_test"
+            }
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_configs_dir(),  "work_dir/configs");
+    }
+
+    #[test]
+    fn test_settings_no_configs_workspace_node() {
+        let json_test_str = r#"
+        {
+            "version": 4
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_configs_dir(),  "work_dir/configs");
+    }
+
+    #[test]
+    fn test_settings_builds_dir() {
+        let json_test_str = r#"
+        {
+            "version": 4,
+            "workspace": {
+              "buildsdir": "builds_test"
+            }
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_builds_dir(),  "work_dir/builds_test");
+    }
+
+    #[test]
+    fn test_settings_no_builds_dir() {
+        let json_test_str = r#"
+        {
+            "version": 4,
+            "workspace": {
+              "artifactsdir": "artifacts_test"
+            }
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_builds_dir(),  "work_dir/builds");
+    }
+
+    #[test]
+    fn test_settings_no_builds_workspace_node() {
+        let json_test_str = r#"
+        {
+            "version": 4
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_builds_dir(),  "work_dir/builds");
+    }
+
+    #[test]
+    fn test_settings_artifacts_dir() {
+        let json_test_str = r#"
+        {
+            "version": 4,
+            "workspace": {
+              "artifactsdir": "artifacts_test"
+            }
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_artifacts_dir(),  "work_dir/artifacts_test");
+    }
+
+    #[test]
+    fn test_settings_no_artifacts_dir() {
+        let json_test_str = r#"
+        {
+            "version": 4,
+            "workspace": {
+              "buildsdir": "builds_test"
+            }
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_artifacts_dir(),  "work_dir/artifacts");
+    }
+
+    #[test]
+    fn test_settings_no_artifacts_workspace_node() {
+        let json_test_str = r#"
+        {
+            "version": 4
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_artifacts_dir(),  "work_dir/artifacts");
+    }
+
+    #[test]
+    fn test_settings_scripts_dir() {
+        let json_test_str = r#"
+        {
+            "version": 4,
+            "workspace": {
+              "scriptsdir": "scripts_test"
+            }
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_scripts_dir(),  "work_dir/scripts_test");
+    }
+
+    #[test]
+    fn test_settings_no_scripts_dir() {
+        let json_test_str = r#"
+        {
+            "version": 4,
+            "workspace": {
+              "buildsdir": "builds_test"
+            }
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_scripts_dir(),  "work_dir/scripts");
+    }
+
+    #[test]
+    fn test_settings_no_scripts_workspace_node() {
+        let json_test_str = r#"
+        {
+            "version": 4
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_scripts_dir(),  "work_dir/scripts");
+    }
+
+    #[test]
+    fn test_settings_docker_dir() {
+        let json_test_str = r#"
+        {
+            "version": 4,
+            "workspace": {
+              "dockerdir": "docker_test"
+            }
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_docker_dir(),  "work_dir/docker_test");
+    }
+
+    #[test]
+    fn test_settings_no_docker_dir() {
+        let json_test_str = r#"
+        {
+            "version": 4,
+            "workspace": {
+              "buildsdir": "builds_test"
+            }
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_docker_dir(),  "work_dir/docker");
+    }
+
+    #[test]
+    fn test_settings_no_docker_workspace_node() {
+        let json_test_str = r#"
+        {
+            "version": 4
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_docker_dir(),  "work_dir/docker");
+    }
+
+    #[test]
+    fn test_settings_cache_dir() {
+        let json_test_str = r#"
+        {
+            "version": 4,
+            "workspace": {
+              "cachedir": "cache_test"
+            }
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_cache_dir(),  "work_dir/cache_test");
+    }
+
+    #[test]
+    fn test_settings_no_cache_dir() {
+        let json_test_str = r#"
+        {
+            "version": 4,
+            "workspace": {
+              "buildsdir": "builds_test"
+            }
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_cache_dir(),  "work_dir/cache");
+    }
+
+    #[test]
+    fn test_settings_no_cache_workspace_node() {
+        let json_test_str = r#"
+        {
+            "version": 4
+        }"#;
+        let settings = helper_settings_from_str(json_test_str);
+        assert_eq!(settings.workspace_cache_dir(),  "work_dir/cache");
     }
 }
